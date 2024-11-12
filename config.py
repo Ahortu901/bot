1aefeb7cb7d1a52264d5dba5977d9374e12878b3
Ahortu901 <aricktech.edu@gmail.com>
2024-11-12 07:16:01 -0500

    update config

--- config.py
+++ config.py
@@ -7,5 +7,8 @@ SECRET_KEY = 'your_secret_key_here'  # If using a broker API
 # Settings for data collection and trading
 TRADING_PAIR = 'EURUSD=X'  # Forex pair
 INTERVAL = 60  # Time interval in seconds between each trade execution cycle
-DATA_PERIOD = '1mo'  # Data collection period (can be '1mo', '1d', etc.)
-DATA_INTERVAL = '1d'  # Data collection interval (e.g., '1d', '1h', '5m')
+DATA_PERIOD = '1y'  # Data collection period (can be '1mo', '1d', etc.)
+DATA_INTERVAL = '1h'  # Data collection interval (e.g., '1d', '1h', '5m')
+
+# Model configuration
+MODEL_PATH = "model/deep_learning_model.h5"  # Path to the saved model
